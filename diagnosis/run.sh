bcadd402af1f723425ece54d3adf5eddc7739ac5
maggusscheppi <74117288+maggusscheppi@users.noreply.github.com>
2023-01-11 19:59:11 +0100

    Delete diagnosis directory

--- diagnosis/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-myNow=$(date +"%T")
-eval echo "$myNow [diagnosis] starting... " $toStartlog
-sleep 30
-myNow=$(date +"%T")
-eval echo "$myNow [diagnosis] starting the monitor" $toStartlog
-python /home/pi/Unitree/autostart/diagnosis/snif_bms.py
-
