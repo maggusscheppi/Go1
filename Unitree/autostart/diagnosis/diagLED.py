8cc98c874651f794c1aa2191fc80104eee4bb38b
Markus Schepp <74117288+maggusscheppi@users.noreply.github.com>
2023-07-17 19:00:41 +0200

    Update diagLED.py
    corrected pack format thx to Noah Lehmann!

--- Unitree/autostart/diagnosis/diagLED.py
+++ Unitree/autostart/diagnosis/diagLED.py
@@ -10,9 +10,9 @@ def alertLED(r,g,b):
 
     mqttc.loop_start()
 
-    infot = mqttc.publish("face_light/color", struct.pack('iii',r,g,b), qos=2)
+    infot = mqttc.publish("face_light/color", struct.pack('BBB',r,g,b), qos=2)
     time.sleep(1)
-    infot = mqttc.publish("face_light/color", struct.pack('iii',0,0,0), qos=2)
+    infot = mqttc.publish("face_light/color", struct.pack('BBB',0,0,0), qos=2)
     time.sleep(1)
     mqttc.disconnect
     # time.sleep(1)
